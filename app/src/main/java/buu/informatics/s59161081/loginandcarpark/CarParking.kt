d5e44046fdd0ed2b4024b63ff9fc8031bf0ea609
kanuntd <kanun.natthanan@gmail.com>
2019-09-03 20:37:46 +0700

    update Carparking

--- app/src/main/java/buu/informatics/s59161081/loginandcarpark/CarParking.kt
+++ app/src/main/java/buu/informatics/s59161081/loginandcarpark/CarParking.kt
@@ -1,4 +1,5 @@
 package buu.informatics.s59161081.loginandcarpark
 
 data class CarParking(val id:Int,var name:String="" ,var carId: String="",
-                      var brand: String="",var status:Boolean=true)
+                      var brand: String="",var status:Boolean=true) {
+}
--- app/src/main/java/buu/informatics/s59161081/loginandcarpark/RoomFragment.kt
+++ app/src/main/java/buu/informatics/s59161081/loginandcarpark/RoomFragment.kt
@@ -1,6 +1,8 @@
 package buu.informatics.s59161081.loginandcarpark
 
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -14,18 +16,52 @@ import buu.informatics.s59161081.loginandcarpark.databinding.FragmentRoomBinding
  * A simple [Fragment] subclass.
  */
 class RoomFragment : Fragment() {
-    private val carParking0 = CarParking(0)
-    private val carParking1 = CarParking(1)
-    private val carParking2 = CarParking(2)
-    private val carParkings = arrayOf(carParking0,carParking1,carParking2)
+    private lateinit var binding: FragmentRoomBinding
+    private val carParking0 = CarParking(1)
+    private val carParking1 = CarParking(2)
+    private val carParking2 = CarParking(3)
+    private val carParkings = arrayOf(carParking0, carParking1, carParking2)
+    private var currentIndex = 0
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<FragmentRoomBinding>(inflater,
+        binding = DataBindingUtil.inflate<FragmentRoomBinding>(inflater,
             R.layout.fragment_room,container,false)
+
+        binding.apply {
+            this.carParking = carParkings?.get(0)
+            roomButton1.setOnClickListener { view ->
+                clickRoom(view)
+            }
+            roomButton2.setOnClickListener { view ->
+                clickRoom(view)
+            }
+            roomButton3.setOnClickListener { view ->
+                clickRoom(view)
+            }
+            updateButton.setOnClickListener{view ->
+                updateRoom(view)
+            }
+
+        }
         setHasOptionsMenu(true)
         return binding.root
+    }
+    private fun getCarIdFormView(view:View):Int {
+        when(view.id) {
+            R.id.roomButton1 -> return 0
+            R.id.roomButton2 -> return 1
+            R.id.roomButton3 -> return 2
+        }
+        return -1
+    }
+    private fun clickRoom(view: View){
+        binding.apply {
+            currentIndex = getCarIdFormView(view)
+            this.carParking = carParkings?.get(currentIndex)
+            invalidateAll()
+        }
     }
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
@@ -36,6 +72,26 @@ class RoomFragment : Fragment() {
         return NavigationUI.onNavDestinationSelected(item!!,
             view!!.findNavController()) || super.onOptionsItemSelected(item)
     }
+
+    private fun updateRoom(view: View){
+        val carPark = carParkings?.get(currentIndex)
+        carPark.status = false
+        binding.apply {
+            if(currentIndex==0){
+                roomButton1.setText(R.string.busy)
+                roomButton1.setBackgroundColor(Color.parseColor("#CCAAAA"))
+            }else if(currentIndex==1){
+                roomButton2.setText(R.string.busy)
+                roomButton2.setBackgroundColor(Color.parseColor("#CCAAAA"))
+            }else{
+                roomButton3.setText(R.string.busy)
+                roomButton3.setBackgroundColor(Color.parseColor("#CCAAAA"))
+            }
+            invalidateAll()
+        }
+
+    }
+
 
 
 
